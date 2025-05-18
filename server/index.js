0f8a08e60476e476011be87ae69c6482b43fa2cf
karan rathour <karanrathore0714@gmail.com>
2025-05-18 21:19:17 +0530

    Initial Commit2

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import courseRoute from "./routes/course.route.js";
 import mediaRoute from "./routes/media.route.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
 import courseProgressRoute from "./routes/courseProgress.route.js";
+import handler from "./routes/hello.js";
 
 dotenv.config({});
 
@@ -28,6 +29,7 @@ app.use(cors({
  
 // apis
 app.use("/api/v1/media", mediaRoute);
+app.use("/api/hello", handler);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/purchase", purchaseRoute);
