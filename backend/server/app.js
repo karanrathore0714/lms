b476d779244de0d2fa0aba5a9bfb5d073b5ee8d9
karan rathour <karanrathore0714@gmail.com>
2025-05-19 21:40:54 +0530

    Initial Commit12

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,13 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import connectDB from "../server/database/db.js";
-import userRoute from "../server/routes/user.route.js";
-import courseRoute from "../server/routes/course.route.js";
-import mediaRoute from "../server/routes/media.route.js";
-import purchaseRoute from "../server/routes/purchaseCourse.route.js";
-import courseProgressRoute from "../server/routes/courseProgress.route.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+import connectDB from './database/db.js';
+import userRoute from './routes/user.route.js';
+import courseRoute from './routes/course.route.js';
+import mediaRoute from './routes/media.route.js';
+import purchaseRoute from './routes/purchaseCourse.route.js';
+import courseProgressRoute from './routes/courseProgress.route.js';
 
 dotenv.config();
 connectDB();
@@ -16,20 +17,19 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(cors({
-    origin: "http://localhost:5173", // change to prod origin for deployment
-    credentials: true
+  origin: 'http://localhost:5173',
+  credentials: true,
 }));
 
-app.use("/api/v1/media", mediaRoute);
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/course", courseRoute);
-app.use("/api/v1/purchase", purchaseRoute);
-app.use("/api/v1/progress", courseProgressRoute);
+app.use('/api/v1/media', mediaRoute);
+app.use('/api/v1/user', userRoute);
+app.use('/api/v1/course', courseRoute);
+app.use('/api/v1/purchase', purchaseRoute);
+app.use('/api/v1/progress', courseProgressRoute);
 
-app.get("/", (req, res) => {
-    res.send("Hi from Express on Vercel");
+app.get('/', (req, res) => {
+  res.send('Hi from Express on Vercel');
 });
 
 export default app;
